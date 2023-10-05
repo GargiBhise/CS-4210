dde733d8bf8bfa68a32dc150028e63f15f601216
GargiBhise <gargib.2496@gmail.com>
2023-10-05 15:08:50 -0700

    Assignment 2 Solutions

---
+++ Assignment 2/naive_bayes.py
@@ -0,0 +1,63 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Gargi Bhise
+# FILENAME: title of the source file
+# SPECIFICATION: description of the program
+# FOR: CS 4210- Assignment #2
+# TIME SPENT: how long it took you to complete the assignment
+#-----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
+# dictionaries, lists, and arrays
+
+#importing some Python libraries
+from sklearn.naive_bayes import GaussianNB
+
+#reading the training data in a csv file
+#--> add your Python code herealcu
+data = []
+with open('Assignment 2/weather_training.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader) # skip header
+    for row in reader:
+        data.append(row[1:])
+
+#transform the original training features to numbers and add them to the 4D array X.
+#For instance Sunny = 1, Overcast = 2, Rain = 3, so X = [[3, 1, 1, 2], [1, 3, 2, 2], ...]]
+#--> add your Python code here
+# X =
+feature_mapping = {'Sunny': 1, 'Overcast': 2, 'Rain': 3, 'Hot': 1, 'Mild': 2, 'Cool': 3, 'High': 1, 'Normal': 2, 'Weak': 1, 'Strong': 2}
+X = [[feature_mapping[feature] for feature in row[:-1]] for row in data]
+
+#transform the original training classes to numbers and add them to the vector Y.
+#For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
+#--> add your Python code here
+# Y =
+class_mapping = {'Yes': 1, 'No': 0}
+Y = [class_mapping[row[-1]] for row in data]
+
+#fitting the naive bayes to the data
+clf = GaussianNB()
+clf.fit(X, Y)
+
+#reading the test data in a csv file
+#--> add your Python code here
+test_data = []
+with open('Assignment 2/weather_test.csv', 'r') as testfile:
+    reader = csv.reader(testfile)
+    next(reader) # skip header
+    for row in reader:
+        test_data.append(row[1:])
+
+#printing the header os the solution
+#--> add your Python code here
+print("Probability1,Probability2,Classification")
+
+#use your test samples to make probabilistic predictions. For instance: clf.predict_proba([[3, 1, 2, 1]])[0]
+#--> add your Python code here
+for row in test_data:
+    test_sample = [feature_mapping[feature] for feature in row[:-1]]
+    prob = clf.predict_proba([test_sample])[0]
+    classification = 'None'
+    if max(prob) >= 0.75:
+        classification = 'Yes' if clf.predict([test_sample])[0] == 1 else 'No'
+    print(f"{prob[0]:.2f},{prob[1]:.2f},{classification}")
